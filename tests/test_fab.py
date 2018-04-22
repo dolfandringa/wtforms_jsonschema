8f89127b6d4fda1c8a86d31576c2d74d39c95383
Dolf Andringa <dolfandringa@gmail.com>
2018-04-22 16:09:35 +0800

    More complex F.A.B. views almost supported

--- tests/test_fab.py
+++ tests/test_fab.py
@@ -1,8 +1,25 @@
+from pprint import pprint
 from collections import OrderedDict
 from wtforms_jsonschema2.fab import FABConverter
 from unittest import TestCase
 from wtforms.form import Form
 from flask_appbuilder.fields import QuerySelectField
+from flask_appbuilder import SQLA, AppBuilder
+from flask import Flask
+from flask_appbuilder import Model
+from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class Gender(object):
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return self.name
 
 
 class FABTestForm(Form):
@@ -10,25 +27,162 @@ class FABTestForm(Form):
         ('type', 'object'),
         ('properties', OrderedDict([
             ('gender', {
-                'type': 'string',
+                'type': 'object',
                 'title': 'Gender',
-                'enum': ['Male', 'Female', 'Alien', 'Other']
+                'enum': [{'id': '1', 'label': 'Male'},
+                         {'id': '2', 'label': 'Female'},
+                         {'id': '3', 'label': 'Alien'},
+                         {'id': '4', 'label': 'Other'}]
             })
         ]))
     ])
     gender = QuerySelectField('Gender',
-                              query_func=lambda: ['Male', 'Female',
-                                                  'Alien', 'Other'])
+                              query_func=lambda: [Gender(1, 'Male'),
+                                                  Gender(2, 'Female'),
+                                                  Gender(3, 'Alien'),
+                                                  Gender(4, 'Other')],
+                              get_pk_func=lambda x: x.id)
+
+
+cfg = {'SQLALCHEMY_DATABASE_URI': 'sqlite:///',
+       'CSRF_ENABLED': False,
+       'WTF_CSRF_ENABLED': False,
+       'SECRET_KEY': 'bla'}
+
+app = Flask('wtforms_jsonschema2_testing')
+app.config.update(cfg)
+db = SQLA(app)
+appbuilder = AppBuilder(app, db.session)
+
+
+class PersonType(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        if self.name:
+            return self.name
+        else:
+            return 'Person Type %s' % self.id
+
+
+class Person(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    dt = Column(DateTime)
+    person_type_id = Column(Integer, ForeignKey('person_type.id'),
+                            nullable=False)
+    person_type = relationship(PersonType, backref='people')
+
+    def __repr__(self):
+        return self.name
+
+
+class Picture(Model):
+    id = Column(Integer, primary_key=True)
+    picture = Column(String, nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'), nullable=False)
+    person = relationship(Person, backref='pictures')
+
+    def __repr__(self):
+        return self.picture
+
+
+class PictureView(ModelView):
+    datamodel = SQLAInterface(Picture)
+    add_columns = ['picture']
+    _pretty_name = 'Picture'
+
+
+class PersonView(ModelView):
+    datamodel = SQLAInterface(Person)
+    related_views = [PictureView]
+    add_columns = ['name', 'dt', 'person_type', 'pictures']
+    _pretty_name = 'Person'
+
+
+class PersonTypeView(ModelView):
+    datamodel = SQLAInterface(PersonType)
+    related_views = [PersonView]
+
+
+appbuilder.add_view(PersonView, 'people')
+appbuilder.add_view(PersonTypeView, 'people types')
+appbuilder.add_view(PictureView, 'pictures')
+
+person_schema = OrderedDict([
+    ('type', 'object'),
+    ('definitions', OrderedDict([
+        ('Person', OrderedDict([
+            ('type', 'object'),
+            ('properties', OrderedDict([
+                ('name', {
+                    'type': 'string',
+                    'title': 'Name'
+                }),
+                ('dt', {
+                    'type': 'string',
+                    'format': 'date-time',
+                    'title': 'Dt'
+                }),
+                ('person_type', {
+                    'title': 'Person Type',
+                    'type': 'object',
+                    'enum': [{'id': '1', 'label': 'male'},
+                             {'id': '2', 'label': 'Person Type 2'}]
+                }),
+                ('pictures', {
+                    'type': 'array',
+                    'title': 'Pictures',
+                    'items': [
+                        {'$ref': '#/defintions/Picture'}
+                    ]
+                })
+            ])),
+            ('required', ['name', 'person_type'])
+        ])),
+        ('Picture', OrderedDict([
+            ('type', 'object'),
+            ('properties', OrderedDict([
+                ('picture', {'title': 'Picture', 'type': 'string'})
+            ])),
+            ('required', ['picture'])
+        ]))
+    ])),
+    ('type', 'object'),
+    ('properties', OrderedDict([
+        ('Person', {'$ref': '#/definitions/Person'})
+    ]))
+
+])
 
 
 class TestFABFormConvert(TestCase):
     def setUp(self):
         self.converter = FABConverter()
         self.maxDiff = None
+        app.testing = True
+        self.app = app.test_client()
+        ctx = app.app_context()
+        ctx.push()
+        db.create_all()
+        db.session.add(PersonType(name='male'))
+        db.session.add(PersonType())
+        db.session.commit()
+        db.session.flush()
 
     def tearDown(self):
-        pass
+        db.drop_all()
+
+    def test_full_view(self):
+        schema = self.converter.convert(PersonView)
+        pprint(schema)
+        pprint(person_schema)
+        self.assertEqual(schema, person_schema)
 
     def test_fab_form(self):
-        self.assertEqual(self.converter.convert(FABTestForm),
+        schema = self.converter.convert(FABTestForm)
+        pprint(schema)
+        pprint(FABTestForm._schema)
+        self.assertEqual(schema,
                          FABTestForm._schema)
--- wtforms_jsonschema2/fab.py
+++ wtforms_jsonschema2/fab.py
@@ -1,7 +1,9 @@
-from flask_appbuilder.fields import QuerySelectField
+from flask_appbuilder.fields import QuerySelectField, QuerySelectMultipleField
+from collections import OrderedDict
 import logging
 from .base import BaseConverter, converts
 from decimal import Decimal
+import re
 
 log = logging.getLogger(__name__)
 
@@ -14,16 +16,140 @@ class FABConverter(BaseConverter):
 
     @converts(QuerySelectField)
     def query_select_field(self, field):
-        choices = field.query_func()
-        if all([isinstance(c, int) for c in choices]):
-            fieldtype = 'integer'
-        elif all([isinstance(c, (float, Decimal, int)) for c in choices]):
-            fieldtype = 'number'
-        else:
-            fieldtype = 'string'
-        options = {'enum': choices}
+        choices = list(field.iter_choices())
+        fieldtype = 'object'
+        options = {'enum': [{'id': c[0], 'label': str(c[1])} for c in choices]}
         required = False
         vals = dict([(v.__class__, v) for v in field.validators])
         required = self._is_required(vals)
 
         return fieldtype, options, required
+
+    @converts(QuerySelectMultipleField)
+    def query_select_multiple_field(self, field):
+        fieldtype = 'array'
+        choices = list(field.iter_choices())
+
+        options = {'items': [{
+            'type': 'object',
+            'enum': [{'id': c[0], 'label': c[1]} for c in choices]
+        }]}
+        required = False
+        vals = dict([(v.__class__, v) for v in field.validators])
+        required = self._is_required(vals)
+
+        return fieldtype, options, required
+
+    def _get_form(self, view, form_type):
+        if form_type == 'add':
+            return view().add_form
+        if form_type == 'edit':
+            return view().edit_form
+
+    def _get_pretty_name(self, view):
+        """
+        Get the pretty name of a view. If defined view._pretty_name will be
+        used, else the name will be derived from the view name by taking the
+        class name and removing  View from the name.
+        """
+        if hasattr(view, '_pretty_name'):
+            return view._pretty_name
+        else:
+            return re.sub('[Vv]iew', '', view.__class__.__name__)
+
+    def convert(self, views, form_type='add'):
+        """
+        Convert a list of Flask Appbuilder ModelViews to JSON Schema.
+        If the ModelView has the optional _pretty_name property, it will be
+        used to refer to the view.
+
+        Each view will be it's own JSON Schema Defition, and referred to by
+        it's pretty name in the properties section.
+
+        Related views are also added as additional definitions and referred to
+        from the "parent" view, but not added to the "properties" part of the
+        schema.
+
+        The idea is that each property in the "properties" section becomes a
+        form in the comsuming application, while defintions that aren't in the
+        properties section are sub-forms to a parent form. This is useful
+        where related views are used to add items in a one-to-many
+        relationship.
+
+        Example:
+        The following model and views
+
+        class Person(Model):
+            id = Column(Integer, primary_key=True)
+            name = Column(String)
+
+        class Picture(Model):
+            id = Column(Integer, primary_key=True)
+            picture = Column(Text)
+            person_id = Column(Integer, ForeignKey(person.id), nullable=False)
+            person = relationship(Person, backref="pictures")
+
+        class PictureView(ModelView):
+            _pretty_name = 'Picture'
+            datamodel = Picture
+            add_columns = ['picture']
+
+        class PersonView(ModelView):
+            _pretty_name = 'Person'
+            datamodel = Person
+            related_views = [PictureView]
+            add_columns = ['name', 'pictures']
+
+        converter.convert({'Person': PersonView})
+
+        should result in the following schema:
+
+
+        {
+            "type": "object",
+            "definitions": {
+                "Person": {
+                    "type": "object",
+                    "properties": {
+                        "name": {"type": "string"},
+                        "pictures": {
+                            "type": "array",
+                            "items": [
+                                {"$ref": "#/definitions/Picture"}
+                            ]
+                        }
+                    }
+                },
+                "Picture": {
+                    "type": "object",
+                    "properties": {
+                        "picture: {"type": "string"}
+                    }
+                }
+            },
+            "properties": {
+                "Person": {"$ref": "#/definitions/Person"}
+            }
+        }
+
+        """
+        schema = OrderedDict([
+            ('type', 'object'),
+            ('definitions', OrderedDict([])),
+            ('properties', OrderedDict([]))
+        ])
+        try:
+            iter(views)
+        except TypeError:
+            views = [views]
+        for view in views:
+            name = self._get_pretty_name(view)
+            schema['definitions'][name] = super().convert(
+                self._get_form(view,  form_type))
+            schema['properties'][name] = {'$ref': '#/definitions/%s' % name}
+
+            for v in view.related_views:
+                name2 = self._get_pretty_name(v)
+                schema['definitions'][name2] = super().convert(
+                    self._get_form(v, form_type))
+        return schema
