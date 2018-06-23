d4e8aab1ba224b23f92a396f9440f76c24f68e2e
Dolf Andringa <dolfandringa@gmail.com>
2018-06-23 23:15:12 +0200

    Add GeoAlchemy2 support.

---
+++ tests/test_geofab.py
@@ -0,0 +1,107 @@
+from pprint import pprint
+from collections import OrderedDict
+from wtforms_jsonschema2.geofab import GeoFABConverter
+from unittest import TestCase
+from flask_appbuilder import AppBuilder
+from fab_geoalchemy.interface import GeoSQLAInterface
+from flask import Flask
+from fab_geoalchemy.views import GeoModelView
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import MetaData, create_engine
+from flask_sqlalchemy import SQLAlchemy
+from geoalchemy2 import Geometry
+import logging
+
+logging.getLogger('wtforms_jsonschema2').setLevel(logging.DEBUG)
+
+cfg = {'SQLALCHEMY_DATABASE_URI': 'postgres:///test',
+       'CSRF_ENABLED': False,
+       'IMG_UPLOAD_URL': '/',
+       'IMG_UPLOAD_FOLDER': '/tmp/',
+       'WTF_CSRF_ENABLED': False,
+       'SECRET_KEY': 'bla'}
+
+app = Flask('wtforms_jsonschema2_geofabtesting')
+app.config.update(cfg)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+metadata = MetaData(bind=engine)
+db = SQLAlchemy(app, metadata=metadata)
+appbuilder = AppBuilder(app, db.session)
+db.session.commit()
+
+
+class GeoObservation(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(Geometry(geometry_type='POINT', srid=4326),
+                      nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+
+class GeoObservationView(GeoModelView):
+    datamodel = GeoSQLAInterface(GeoObservation)
+    add_columns = ['name', 'location']
+    show_title = 'GeoObservation'
+    add_title = 'Add GeoObservation'
+
+
+appbuilder.add_view(GeoObservationView, 'observations')
+
+observation_schema = OrderedDict([
+    ('type', 'object'),
+    ('definitions', OrderedDict([
+        ('GeoObservation', OrderedDict([
+            ('type', 'object'),
+            ('properties', OrderedDict([
+                ('name', {
+                    'type': 'string',
+                    'title': 'Name'
+                }),
+                ('location', OrderedDict([
+                    ('type', 'object'),
+                    ('properties', OrderedDict([
+                        ('lat', {
+                            'type': 'number',
+                            'title': 'Latitude',
+                        }),
+                        ('lon', {
+                            'type': 'number',
+                            'title': 'Longitude'
+                        })
+                    ])),
+                    ('required', ['lat', 'lon']),
+                    ('title', 'Location')
+                ])),
+            ])),
+            ('required', ['name'])
+        ]))
+    ])),
+    ('type', 'object'),
+    ('properties', OrderedDict([
+        ('GeoObservation', {'$ref': '#/definitions/GeoObservation'})
+    ]))
+
+])
+
+
+class TestGeoFABFormConvert(TestCase):
+    def setUp(self):
+        self.converter = GeoFABConverter()
+        self.maxDiff = None
+        app.testing = True
+        self.app = app.test_client()
+        ctx = app.app_context()
+        ctx.push()
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.drop_all()
+
+    def test_full_view(self):
+        schema = self.converter.convert(GeoObservationView)
+        pprint(schema)
+        pprint(observation_schema)
+        self.assertEqual(schema, observation_schema)
---
+++ wtforms_jsonschema2/geofab.py
@@ -0,0 +1,65 @@
+import logging
+from .base import converts
+from .fab import FABConverter
+from wtforms.form import Form
+from wtforms.fields import FormField
+from fab_geoalchemy import PointField
+from copy import deepcopy
+
+
+log = logging.getLogger(__name__)
+
+
+class GeoFABConverter(FABConverter):
+    """
+    Extends the FABConverter with GeoAlchemy2 support for geographic data.
+    """
+
+    def convert(self, views, form_type='add'):
+        log.debug("Calling GeoFABConverter.convert")
+        if isinstance(views, Form) or issubclass(views, Form):
+            return super(FABConverter, self).convert(views)
+        newviews = []
+        try:
+            iter(views)
+        except TypeError:
+            views = [views]
+        for view in views:
+            if isinstance(view, Form):
+                form = view
+                newviews.append(view)
+            else:
+                view = view()
+                newviews.append(view)
+                form = self._get_form(view, form_type)
+            for fname in dir(form):
+                if fname.startswith('_'):
+                    continue
+                field = getattr(form, fname)
+                if fname in self.skip_fields:
+                    continue
+                log.debug('Checking field {}'.format(fname))
+                if hasattr(field, 'field_class') and \
+                        field.field_class == PointField:
+                    log.debug("{} is a pointfield".format(fname))
+                    delattr(form, fname)
+                    latfield = deepcopy(field)
+                    latfield.args = ['Latitude']+list(field.args)[1:]
+                    lonfield = deepcopy(field)
+                    lonfield.args = ['Longitude']+list(field.args)[1:]
+
+                    class subform(Form):
+                        pass
+                    subform.lat = latfield
+                    subform.lon = lonfield
+                    setattr(form, fname, FormField(subform))
+        return super().convert(newviews, form_type)
+
+    @converts(PointField)
+    def convert_point_field(self, field):
+        fieldtype = 'number'
+        options = {}
+        vals = dict([(v.__class__, v) for v in field.validators])
+        required = self._is_required(vals)
+
+        return fieldtype, options, required
