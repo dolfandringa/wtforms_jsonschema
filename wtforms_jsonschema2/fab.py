653f7e2aa83f934197e75da310f20911a7573638
Dolf Andringa <dolfandringa@gmail.com>
2018-10-06 12:30:15 +0800

    working on supporting conditions

--- wtforms_jsonschema2/fab.py
+++ wtforms_jsonschema2/fab.py
@@ -4,7 +4,7 @@ from flask_appbuilder.upload import ImageUploadField
 from collections import OrderedDict
 import logging
 from .base import BaseConverter, converts
-import re
+from .utils import _get_pretty_name, _get_related_view_property
 from wtforms.form import Form
 
 
@@ -72,26 +72,6 @@ class FABConverter(BaseConverter):
         if form_type == 'edit':
             return view.edit_form
 
-    def _get_pretty_name(self, view, form_type):
-        """
-        Get the pretty name of a view. If defined view._pretty_name will be
-        used, else the name will be derived from the view name by taking the
-        class name and removing  View from the name.
-        """
-        if isinstance(view, type):
-            # Instantiate the view if not done already
-            view = view()
-        if form_type == 'add' and view.add_title != '':
-            return view.add_title
-        elif form_type == 'show' and view.show_title != '':
-            return view.show_title
-        elif form_type == 'list' and view.list_title != '':
-            return view.list_title
-        elif form_type == 'edit' and view.edit_title != '':
-            return view.edit_title
-        else:
-            return re.sub('[Vv]iew', '', view.__class__.__name__)
-
     def convert(self, views, form_type='add'):
         """
         Convert a list of Flask Appbuilder ModelViews to JSON Schema.
@@ -111,70 +91,8 @@ class FABConverter(BaseConverter):
         where related views are used to add items in a one-to-many
         relationship.
 
-        Example:
-        The following model and views
-
-        class Person(Model):
-            id = Column(Integer, primary_key=True)
-            name = Column(String)
-
-        class Picture(Model):
-            id = Column(Integer, primary_key=True)
-            picture = Column(Text)
-            person_id = Column(Integer, ForeignKey(person.id), nullable=False)
-            person = relationship(Person, backref="pictures")
-
-        class PictureView(ModelView):
-            list_title = 'Pictures'
-            add_title = 'Add Picture'
-            edit_title = 'Edit Picture'
-            show_title = 'Picture'
-            datamodel = Picture
-            add_columns = ['picture']
-
-        class PersonView(ModelView):
-            show_title = 'Person'
-            edit_title = 'Edit Person'
-            add_title = 'Add Person'
-            list_title = 'People'
-            datamodel = Person
-            related_views = [PictureView]
-            add_columns = ['name']
-
-        converter.convert({'Person': PersonView})
-
-        should result in the following schema:
-
-
-        {
-            "type": "object",
-            "definitions": {
-                "Person": {
-                    "type": "object",
-                    "properties": {
-                        "name": {"type": "string"},
-                        "pictures": {
-                            "type": "array",
-                            "title": "Pictures",
-                            "items": [
-                                {"$ref": "#/definitions/Picture"}
-                            ]
-                        }
-                    }
-                },
-                "Picture": {
-                    "type": "object",
-                    "properties": {
-                        "picture: {"type": "string"}
-                    }
-                }
-            },
-            "properties": {
-                "Person": {"$ref": "#/definitions/Person"}
-            }
-        }
-
         """
+
         if not isinstance(views, list) and (isinstance(views, Form)
                                             or issubclass(views, Form)):
             return super().convert(views)
@@ -188,7 +106,7 @@ class FABConverter(BaseConverter):
             ('properties', OrderedDict([]))
         ])
         for view in views:
-            name = self._get_pretty_name(view, 'show')
+            name = _get_pretty_name(view, 'show')
             schema['definitions'][name] = super().convert(
                 self._get_form(view,  form_type))
             schema['properties'][name] = {'$ref': '#/definitions/%s' % name}
@@ -196,18 +114,14 @@ class FABConverter(BaseConverter):
                 continue
             for v in view.related_views:
                 for f in v.datamodel.get_related_fks([view]):
-                    defin = {}
+                    defin = _get_related_view_property(view, v, f)
+                    obj_name = _get_pretty_name(v, 'show')
                     schema['definitions'][name]['properties'][f] = defin
-                    if view.datamodel.is_relation_one_to_many(f):
-                        title = self._get_pretty_name(v, 'list')
-                        obj_name = self._get_pretty_name(v, 'show')
-                        ref = '#/definitions/%s' % obj_name
-                        defin['type'] = 'array'
-                        defin['title'] = title
-                        defin['items'] = [{'$ref': ref}]
-                    else:
-                        title = obj_name = self._get_pretty_name(v, 'show')
-                        defin['$ref'] = '#/definitions/%s' % obj_name
                     schema['definitions'][obj_name] = super().convert(
                         self._get_form(v, form_type))
+            if hasattr(view, '_conditional_relations'):
+                for condition in view._conditional_relations:
+                    ckey, cval = condition.get_json_schema(view)
+                    schema['definitions'][name][ckey] = cval
+
         return schema
